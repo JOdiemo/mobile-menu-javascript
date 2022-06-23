1249c045fef098480b02fa161198c34647c0b1f4
JOdiemo <odiemo@gmail.com>
2022-06-23 10:00:47 +0300

    Added the menu

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+
+function showMenu() {
+  document.querySelector('.navigation').classList.toggle('active');
+  document.querySelector('.menu-list').classList.toggle('active');
+}
+
+// eslint-disable-next-line no-unused-vars
+function hideMenu() {
+  document.querySelector('.navigation.active').classList.remove('active');
+  document.querySelector('.menu-list.active').classList.remove('active');
+
+}
